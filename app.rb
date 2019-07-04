22e314d70a1a595f1f37ed16e2f897877e0a2591
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-04 02:07:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,19 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-    get '/' do
+  get '/' do
       erb :index
     end
-
+  
     get '/new' do
       erb :create_puppy
     end
-
+  
     post '/puppy' do
-     @name=params[:name]
+      @name=params[:name]
       @breed=params[:breed]
       @age=params[:age]
       erb :display_puppy
-    end
-end
+    end  
+  end
