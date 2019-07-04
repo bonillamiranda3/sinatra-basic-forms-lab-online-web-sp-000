a9fa0d303c3153df22a395cc754fda0a046c9fcc
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-04 01:51:35 +0000

    Automatically backed up by Learn

---
+++ models/puppy.rb
@@ -0,0 +1,11 @@
+class puppy
+
+  attr_accessor :age, :name
+  attr_reader :breed
+
+  def initialize(name:, breed:, months_old:)
+    @name = name
+    @breed = breed
+    @age = months_old
+  end
+end  
