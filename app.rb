768017add452d2588c8cd7f7356e6132c9fbb1cc
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-04 02:07:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ class App < Sinatra::Base
   get '/' do
       erb :index
     end
-  
+
     get '/new' do
       erb :create_puppy
     end
-  
+
     post '/puppy' do
       @name=params[:name]
       @breed=params[:breed]
       @age=params[:age]
       erb :display_puppy
-    end  
+    end
   end
